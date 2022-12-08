5800d1aae0d19601ba8e5bff7fbfa3fb07ac7560
hitokizzy <hitokizzy@gmail.com>
2022-12-08 11:40:41 +0700

    Initial commit

---
+++ geezram/__init__.py
@@ -0,0 +1,41 @@
+from geezram.core.bot import AnonXBot
+from geezram.core.dir import dirr
+from geezram.core.git import git
+from geezram.core.userbot import Userbot
+from geezram.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AnonXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
